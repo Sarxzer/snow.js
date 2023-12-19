a6375608226f8e6785ad716db1bb4ff5e6e3f09b
Sarxzer <80457483+Sarxzer@users.noreply.github.com>
2023-12-19 23:36:55 +0100

    Update snow.js

--- snow.js
+++ snow.js
@@ -12,6 +12,14 @@ function snow(ammount) {
       const driftDelay = Math.random() * 4; // Délai de 0 à 4 secondes
       const startingX = Math.random() * window.innerWidth; // Position horizontale aléatoire
       const size = Math.random() * 10 + 5; // Taille de 5 à 15 pixels
+        
+      flake.style.top = '-10px';
+      flake.styke.zIndex = '9999';
+      flake.style.userSelect = 'none';
+      flake.style.cursor = 'default';
+      flake.style.pointerEvent =  'none';
+      flake.style.color = '#ffffff';
+      
 
       flake.style.left = `${startingX}px`;
       flake.style.animation = `fall ${driftDuration}s linear infinite`;
